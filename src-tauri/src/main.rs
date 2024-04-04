007be216ffd375a82b444c3ca7b511a4a851da3e
Tanseer <hashiradnan55@gmail.com>
2024-04-04 23:25:27 +0500

    "Database integrated for keyboard"

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::process::Command;
+use std::path::PathBuf;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,8 +11,18 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+
+    let script_path = PathBuf::from("..").join("backend").join("main.py");
+
+    // Run the Python script using an external command
+    Command::new("python")
+        .arg(script_path)
+        .spawn()
+        .expect("failed to execute Python script");
+    
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+    
 }
